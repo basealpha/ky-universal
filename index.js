6e2e0e47b3ce9f242392261b6e5226e7e5496dcd
Camille Lahoud <camille.lahoud@basealpha.io>
2023-05-19 11:38:23 +0200

    Avoiding top level await but keeping the async

--- index.js
+++ index.js
@@ -33,13 +33,32 @@ if (!globalThis.ReadableStream) {
 
 // import { default as ky, HTTPError, TimeoutError } from "ky";
 // const {default: ky, HTTPError, TimeoutError} = await import('ky');
-
-async function loadKy() {
-	const { default: ky, HTTPError, TimeoutError } = await import("ky");
-	return { ky, HTTPError, TimeoutError };
+let importPromise;
+async function importKy() {
+	if (!importPromise) {
+		importPromise = await import("ky");
+	}
+	return importPromise;
 }
 
-const { ky, HTTPError, TimeoutError } = loadKy();
+async function loadKy() {
+	const { default: ky } = await importKy();
+	return ky;
+}
+
+async function loadHTTPError() {
+	const { HTTPError } = await importKy();
+	return HTTPError;
+}
+
+async function loadTimeoutError() {
+	const { TimeoutError } = await importKy();
+	return TimeoutError;
+}
+
+const { ky } = loadKy();
+const { HTTPError } = loadHTTPError();
+const { TimeoutError } = loadTimeoutError();
 
 export default ky;
 export { HTTPError, TimeoutError };
